88b6e4c62716b11c5100de1069b45c668ad2d65c
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-08-16 14:52:25 -0400

    add views

--- movielens/flix/views.py
+++ movielens/flix/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    pass
+
+
+def movie(request):
+    pass
+
+
+def rater(request):
+    pass
+
+
+def signin(request):
+    pass
+
+
+def signout(request):
+    pass
+
+
+def register(request):
+    pass
+    
