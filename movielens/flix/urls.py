facdc2f00b72f9c198784545c71d9236fe2c3f9c
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-08-16 15:07:14 -0400

    fixed urls

--- movielens/flix/urls.py
+++ movielens/flix/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^movie/([0-9]+)/$', views.movie, name='movie'),
     url(r'^rater/([0-9]+)/$', views.rater, name='rater'),
     url(r'^login/$', views.signin, name='login'),
