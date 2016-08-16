94f4c0aaf8dfa8d07d09994f71f27721f7f783a7
B. Maxwell Ruotsi <bmruotsi@gmail.com>
2016-08-16 14:46:38 -0400

    broken models

--- movielens/flix/models.py
+++ movielens/flix/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=240)
+    genre = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.title
+
+class Raters(models.Model):
+    occupations = [
+        (0, "other")
+        (1, "academic/educator"
+        (2, "artist"
+        (3, "clerical/admin"
+        (4, "college/grad student"
+        (5, "customer service"
+        (6, "doctor/health care"
+        (7, "executive/managerial"
+        (8, "farmer"
+        (9, "homemaker"
+        (10, "K-12 student"
+        (11, "lawyer"
+        (12, "programmer"
+        (13, "retired"
+        (14, "sales/marketing"
+        (15, "scientist"
+        (16, "self-employed"
+        (17, "technician/engineer"
+        (18, "tradesman/craftsman"
+        (19, "unemployed"
+        (20, "writer"
+    ]
+
+
+    gender = model.CharField(max_length=10)
+    age = model.IntegerField()
+    occupation = model.IntegerField()
+    zipcode = model.
+
+class Rating(models.Model):
