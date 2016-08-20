6bd97a5fb72c292217f6c054c4aa2fef3ae0594d
B. Maxwell Ruotsi <bmruotsi@gmail.com>
2016-08-20 14:06:13 -0400

    search

--- movielens/flix/views.py
+++ movielens/flix/views.py
@@ -50,64 +50,79 @@ def search_page(request, search, search_results):
 
 
 def movie(request, movie_id):
-    movie = get_object_or_404(Movie, pk=movie_id)
-    ratings = Rating.objects.filter(movie_id=movie.id)
-    avg_rating = round(movie.rating_set.aggregate(Avg('rating'))['rating__avg'], 2)
-    trailer = youtube_search("{} trailer".format(movie.title))[0]
-    context = {
-        'movie': movie,
-        'ratings': ratings,
-        'avg_rating': avg_rating,
-        'trailer': trailer
-    }
-    return render(request, 'flix/movie.html', context)
+    if request.POST:
+        return search(request, request.POST["search"])
+    else:
+        movie = get_object_or_404(Movie, pk=movie_id)
+        ratings = Rating.objects.filter(movie_id=movie.id)
+        avg_rating = round(movie.rating_set.aggregate(Avg('rating'))['rating__avg'], 2)
+        trailer = youtube_search("{} trailer".format(movie.title))[0]
+        context = {
+            'movie': movie,
+            'ratings': ratings,
+            'avg_rating': avg_rating,
+            'trailer': trailer
+        }
+        return render(request, 'flix/movie.html', context)
 
 
 def rater(request, rater_id):
-    rater = get_object_or_404(Rater, pk=rater_id)
-    ratings = Rating.objects.filter(rater_id=rater_id)
-    movies_rated = []
-    for movie in ratings:
-        title = Movie.objects.get(id=movie.movie_id)
-        movies_rated.append((title, movie.movie_id, movie.rating))
-    context = {
-        'rater': rater,
-        'movies_rated': movies_rated
-    }
-    return render(request, 'flix/rater.html', context)
+    if request.POST:
+        return search(request, request.POST["search"])
+    else:
+        rater = get_object_or_404(Rater, pk=rater_id)
+        ratings = Rating.objects.filter(rater_id=rater_id)
+        movies_rated = []
+        for movie in ratings:
+            title = Movie.objects.get(id=movie.movie_id)
+            movies_rated.append((title, movie.movie_id, movie.rating))
+        context = {
+            'rater': rater,
+            'movies_rated': movies_rated
+        }
+        return render(request, 'flix/rater.html', context)
 
 
 def signin(request):
     if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Where should we sent the user?
-            # T: good question, probably their own profile page or the index
-            return HttpResponseRedirect(reverse(rater, args=[request.user.id]))
+        if "search" in request.POST:
+            return search(request, request.POST["search"])
+        else:
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Where should we sent the user?
+                # T: good question, probably their own profile page or the index
+                return HttpResponseRedirect(reverse(rater, args=[request.user.id]))
     else:
         return render(request, "flix/login.html", {})
 
 
 def signout(request):
-    logout(request)
-    return render(request, 'flix/logout.html')
+    if request.POST:
+        return search(request, request.POST["search"])
+    else:
+        logout(request)
+        return render(request, 'flix/logout.html')
 
 
 def register(request):
     if request.method == 'POST':
-        rater_form = RaterForm(request.POST, prefix='rater')
-        user_form = UserCreationForm(request.POST, prefix='user')
-        if rater_form.is_valid() * user_form.is_valid():
-            user = user_form.save(commit=False)
-            user.save()
-            rater = rater_form.save(commit=False)
-            rater.user_id = user.id
-            rater.id = user.id
-            rater.save()
-            return HttpResponseRedirect('/')
+        if "search" in request.POST:
+            return search(request, request.POST["search"])
+        else:
+            rater_form = RaterForm(request.POST, prefix='rater')
+            user_form = UserCreationForm(request.POST, prefix='user')
+            if rater_form.is_valid() * user_form.is_valid():
+                user = user_form.save(commit=False)
+                user.save()
+                rater = rater_form.save(commit=False)
+                rater.user_id = user.id
+                rater.id = user.id
+                rater.save()
+                return HttpResponseRedirect('/')
     else:
         rater_form = RaterForm(prefix='rater')
         user_form = UserCreationForm(prefix='user')
@@ -116,7 +131,9 @@ def register(request):
 
 
 def get_new_rating(request, movie_id):
-    if request.user.is_authenticated:
+    if "search" in request.POST:
+        return search(request, request.POST["search"])
+    elif request.user.is_authenticated:
         if request.method == 'POST':
             rating_form = RatingForm(request.POST, prefix='rating')
             if rating_form.is_valid():
